77cdcabb6eeee223dff4dc6e1ba796af85eaf23e
Adaran1337 <matveev.aleksandr_1337@mail.ru>
2022-06-30 10:00:26 +0300

    Initial commit

---
+++ src/test/java/com/company/bookingresourses/BookingResoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.bookingresourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingResoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
